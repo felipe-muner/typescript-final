0268aa67190eb4e92144125a165de149fb0eed88
felipemuner <felipe.muner@gmail.com>
2021-06-20 16:12:39 +0700

    purging

--- components/date.tsx
+++
@@ -1,8 +0,0 @@
-// import { parseISO, format } from 'date-fns'
-
-export default function Date({ dateString }: { dateString: string }) {
-  // const date = parseISO(dateString)
-  // return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
-  const date = dateString;
-  return <time dateTime={dateString}>{date}</time>;
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,31 +3,25 @@ import Head from "next/head";
 import Channels from "../components/channels";
 import Messages from "../components/messages";
 import SendMessage from "../components/sendMessage";
-import { IChannel, IMessage} from "../types/index";
+import { IChannel, IMessage } from "../types/index";
 import Api from "../api";
-import utilStyles from "../styles/utils.module.css";
-import { useState,useEffect } from "react";
+import classes from "../styles/utils.module.css";
+import { useState, useEffect } from "react";
 
 export default function Home({ allChannels }: { allChannels: IChannel[] }) {
-  // const [loading, setLoading] = useState<Boolean>(false);
   const [channel, setChannel] = useState<IChannel>();
   const [messages, setMessages] = useState<IMessage[]>();
-  // const user: IFullName = { firstName: "felipe", lastName: "muner" };
-  // const myPost: IPost = { id: "filename", body: "body", title: "title" };
-  // <ShowDetails user={user} myPost={myPost}></ShowDetails>
 
-  const addMsg = (q: {msg: string}) => {
+  const addMsg = (q: { msg: string }) => {
     setMessages((prevState: any) => [
       ...prevState,
       { msgId: Math.random().toString(), content: q.msg },
     ]);
-  }
+  };
 
   async function getMessagesById() {
-    // setLoading(true)
-    const filteredList = await Api.message.getMessagesById(channel);
-    setMessages(filteredList);
-    // setLoading(false)
+    const list: IMessage[] = await Api.message.getMessagesById(channel);
+    setMessages(list);
   }
 
   useEffect(() => {
@@ -38,14 +32,14 @@ export default function Home({ allChannels }: { allChannels: IChannel[] }) {
 
   return (
     <div>
-      <div className={utilStyles.container}>
-        <div className={utilStyles.channel}>
+      <div className={classes.container}>
+        <div className={classes.channel}>
           <Channels channels={allChannels} setChannel={setChannel} />
         </div>
-        <div className={utilStyles.messages}>
+        <div className={classes.messages}>
           <Messages channel={channel} messages={messages} />
         </div>
-        <div className={utilStyles.form}>
+        <div className={classes.form}>
           <SendMessage channel={channel} addMsg={addMsg} />
         </div>
       </div>
