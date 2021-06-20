8ef662f61d21967de458d3fb3d56b0e6c7df73da
felipemuner <felipe.muner@gmail.com>
2021-06-20 13:45:51 +0700

    set right ts for channelId number

--- pages/api/messages/[channelId].js
+++
@@ -1,7 +0,0 @@
-import { channels, msgs } from "../serverJson";
-
-export default function handler(req, res) {
-  const { channelId } = req.query;
-  const filtered = msgs.filter((m) => m.channelId === parseInt(channelId));
-  res.status(200).json(filtered);
-}
