a5f90bf0784c9ffd17df966485bb77dea11eb9e7
felipemuner <felipe.muner@gmail.com>
2021-06-20 14:05:41 +0700

    set layout for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,22 @@ import SendMessage from "../components/sendMessage";
 import ShowDetails from "../components/showDetails";
 import { IChannel, IFullName, IPost } from "../types/index";
 import Api from "../api";
-
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home({ allChannels }: { allChannels: IChannel[] }) {
   // const user: IFullName = { firstName: "felipe", lastName: "muner" };
   // const myPost: IPost = { id: "filename", body: "body", title: "title" };
   //   <ShowDetails user={user} myPost={myPost}></ShowDetails>
-  
+
   return (
     <div>
       <h1>13</h1>
       <h1>{JSON.stringify(allChannels)}</h1>
+      <div className={utilStyles.container}>
+        <div>Left div</div>
+        <div>Middle div</div>
+        <div>Right div</div>
+      </div>
     </div>
   );
 }
