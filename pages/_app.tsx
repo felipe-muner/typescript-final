cedf534353c79eb21ad0522ba56746b56e0f6b83
felipemuner <felipe.muner@gmail.com>
2021-06-20 16:04:26 +0700

    set SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/global.css'
-import { AppProps } from 'next/app'
+import "../styles/global.css";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Felipe Muner - Alphasense task</title>
+        <meta name="description" content="Alphasense - Felipe Muner task" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
