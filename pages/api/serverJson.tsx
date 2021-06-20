3305d053b6647086f9ef28763a45582860d9f1ca
felipemuner <felipe.muner@gmail.com>
2021-06-20 16:02:15 +0700

    cleaning db

--- pages/api/serverJson.tsx
+++ pages/api/serverJson.tsx
@@ -9,45 +9,46 @@ export let channels: IChannel[] = [
 
 export let autoIncrement = { id: 1 };
 
-export let msgs: IMessage[] = [
-  {
-    msgId: autoIncrement.id++,
-    channelId: 1,
-    content: "ndustry. Lorem Ipsum has been the industry's ",
-  },
-  {
-    msgId: autoIncrement.id++,
-    channelId: 2,
-    content: "nt of a page when looking at its ",
-  },
-  {
-    msgId: autoIncrement.id++,
-    channelId: 1,
-    content: "ook like readable English. Many desktop publishin",
-  },
-  {
-    msgId: autoIncrement.id++,
-    channelId: 2,
-    content: "odel text, and a search for 'lorem i",
-  },
-  {
-    msgId: autoIncrement.id++,
-    channelId: 3,
-    content: "ook like readable English. Many desktop publishin",
-  },
-  {
-    msgId: autoIncrement.id++,
-    channelId: 3,
-    content: "odel text, and a search for 'lorem i",
-  },
-  // {
-  //   msgId: autoIncrement.id++,
-  //   channelId: 4,
-  //   content: "ndustry. Lorem Ipsum has been the industry's ",
-  // },
-  {
-    msgId: autoIncrement.id++,
-    channelId: 1,
-    content: "nt of a page when looking at its ",
-  },  
-];
+export let msgs: IMessage[] = []
+// export let msgs: IMessage[] = [
+//   {
+//     msgId: autoIncrement.id++,
+//     channelId: 1,
+//     content: "ndustry. Lorem Ipsum has been the industry's ",
+//   },
+//   {
+//     msgId: autoIncrement.id++,
+//     channelId: 2,
+//     content: "nt of a page when looking at its ",
+//   },
+//   {
+//     msgId: autoIncrement.id++,
+//     channelId: 1,
+//     content: "ook like readable English. Many desktop publishin",
+//   },
+//   {
+//     msgId: autoIncrement.id++,
+//     channelId: 2,
+//     content: "odel text, and a search for 'lorem i",
+//   },
+//   {
+//     msgId: autoIncrement.id++,
+//     channelId: 3,
+//     content: "ook like readable English. Many desktop publishin",
+//   },
+//   {
+//     msgId: autoIncrement.id++,
+//     channelId: 3,
+//     content: "odel text, and a search for 'lorem i",
+//   },
+//   {
+//     msgId: autoIncrement.id++,
+//     channelId: 4,
+//     content: "ndustry. Lorem Ipsum has been the industry's ",
+//   },
+//   {
+//     msgId: autoIncrement.id++,
+//     channelId: 1,
+//     content: "nt of a page when looking at its ",
+//   },  
+// ];
